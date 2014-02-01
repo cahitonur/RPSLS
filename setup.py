8b6d18f70e9ecdaade8aa67135cb351b11859502
Cahit Onur <cahitonur@gmail.com>
2014-02-02 04:29:19 +0530

    uwsgi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 requires = [
     'pyramid',
     'pyramid_debugtoolbar',
-    'uwsgi', 'pyramid_jinja2'
+    'waitress', 'pyramid_jinja2'
 ]
 
 setup(name='RPSLS',
